825a671f91d1e54eb6f5a39eaf4441813bd0ed78
tijira-1999 <tijira1999sahoo@gmail.com>
2020-07-22 19:32:51 +0530

    use case 2 completed

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-read -p "Enter first name " name
-namePattern="^[A-Z]{1}[a-zA-Z]{2,}$"
-if [[ $name =~ $namePattern ]]
+read -p "Enter first name " fname
+fnamePattern="^[A-Z]{1}[a-zA-Z]{2,}$"
+if [[ $fname =~ $fnamePattern ]]
 then
 	echo "valid first name"
 else
 	echo "invalid first name"
 fi
+
+read -p "Enter last name " lname
+lnamePattern="^[A-Z]{1}[a-zA-Z]{2,}$"
+if [[ $lname =~ $lnamePattern ]]
+then
+        echo "valid last name"
+else
+        echo "invalid last name"
+fi
+
